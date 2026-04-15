cc3280d2dfec3ffef0f5670c72eccdca545cb8cc
Suraj931525 <surajpatil9315@gmail.com>
2026-04-15 12:28:34 +0530

    add javascrip basic program link external javascript file in html file also learn data type number method and taking input from user

---
+++ Javascript/datatype.js
@@ -0,0 +1,94 @@
+let a="ForTune"
+console.log(a);
+
+let b=a.charAt(4)
+console.log(b);
+
+let z=a.length
+console.log(z);
+
+let c=a.charCodeAt(4)
+console.log(c);
+
+let d=a.startsWith("F")
+console.log(d);
+
+let e=a.endsWith("e")
+console.log(e);
+
+let f=a.includes("o")
+console.log(f);
+
+
+
+let g=a.toLocaleUpperCase()
+console.log(g);
+
+let h=a.toLocaleLowerCase()
+console.log(h);
+
+let i=a.concat(" Cloud")
+console.log(i)
+
+
+let j=a.replace("ForTune", "Cravita")
+console.log(j)
+
+let k=a.repeat(3)
+console.log(k)
+
+ let m=a.split("u")
+ console.log(m) 
+
+    let n=a.substring(1,4)
+    console.log(n)
+
+    let o=a.trim()
+    console.log(o)
+
+    let p=a.trimStart()
+    console.log(p)
+
+    let q=a.trimEnd()
+    console.log(q)
+
+    let r=a.valueOf()
+    console.log(r)
+
+
+
+
+    // number method
+
+
+    let u=56.235689
+    console.log(u);
+    console.log(typeof(u));
+
+
+    let v=u.toFixed(2)
+    console.log(v);
+    console.log(typeof(v));
+
+    let x=u.toString()
+    console.log(x);
+    console.log(typeof(x));
+
+
+
+    // boolean method
+
+    let y=true
+    console.log(y);
+    console.log(typeof(y));
+
+
+    //undifined and null
+    let l;
+    console.log(l);
+    console.log(typeof(l));
+
+    let s=null
+
+    console.log(s);
+    console.log(typeof(s));
---
+++ Javascript/day1/task1.js
@@ -0,0 +1,23 @@
+
+
+a=10;
+document.writeln(a);
+
+let b="fortune cloud";
+document.writeln(b);
+
+let c=34.56
+console.log(c);
+
+
+// add two numbers
+let x=5;
+let y=10;
+console.log(x+y);
+
+
+//take input from user
+let m=parseInt(prompt("Enter first number:"));
+let n=parseInt(prompt("Enter second number:"));
+
+document.writeln(m+n);
